44a98cec207c7e94fd98b6693dbaa815dce53414
savitaavenkat <45669609+savitaavenkat@users.noreply.github.com>
2020-05-28 19:58:13 -0400

    Update Balanced_Brackets.py

--- Data_Structures/Balanced_Brackets.py
+++ Data_Structures/Balanced_Brackets.py
@@ -3,41 +3,37 @@
 # Date code accepted for submission on hackerrank: May 28th 2020
 
 # Problem statement:
+  ------------------
 #A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
-
 #Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {) occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same type. There are three types of matched pairs of brackets: [], {}, and ().
-
 #A matching pair of brackets is not balanced if the set of brackets it encloses are not matched. For example, {[(])} is not balanced because the contents in between { and } are not balanced. The pair of square brackets encloses a single, unbalanced opening bracket, (, and the pair of parentheses encloses a single, unbalanced closing square bracket, ].
-
 #By this logic, we say a sequence of brackets is balanced if the following conditions are met:
-
 #It contains no unmatched brackets.
 #The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched pair of brackets.
 #Given  strings of brackets, determine whether each sequence of brackets is balanced. If a string is balanced, return YES. Otherwise, return NO.
 
 # Function Description:
-
+  ---------------------
 #Complete the function isBalanced in the editor below. It must return a string: YES if the sequence is balanced or NO if it is not.
-
 #isBalanced has the following parameter(s):
-
 #s: a string of bracket
 
 # Input Format:
-
+  -------------
 #The first line contains a single integer , the number of strings.
 #Each of the next  lines contains a single string , a sequence of brackets.
 
 # Constraints:
+  ------------
 #All chracters in the sequences ∈ { {, }, (, ), [, ] }.
 
 # Output Format:
-
-For each string, return YES or NO.
+  --------------
+#For each string, return YES or NO.
 
 
 # Solution:
-
+  ---------
 import math
 import os
 import random
